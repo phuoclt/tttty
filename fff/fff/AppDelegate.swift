ab1e9cd653cba5db7ac47e73c8d0010709957992
PhuocLT <tuphuoc90@gmail.com>
2017-03-07 23:10:53 +0700

    t2

--- fff/fff/AppDelegate.swift
+++ fff/fff/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        dsfdsfsdfdsfds
+        //t2
         // Override point for customization after application launch.
         return true
     }
