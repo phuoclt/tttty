add17753000f4bda7441121348266e56472c07b7
PhuocLT <tuphuoc90@gmail.com>
2017-03-07 23:37:43 +0700

    hoathuongphuoc

--- fff/fff/AppDelegate.swift
+++ fff/fff/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //t1
         //t11
+        //hoathuongphuoc
         // Override point for customization after application launch.
     print("fdfdsfs")
         return true
